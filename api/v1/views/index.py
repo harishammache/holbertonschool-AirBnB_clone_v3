0b8c0e8283b4e947e653e4f5c8242c223f7da390
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-04-16 14:26:44 +0000

    add classe to count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
@@ -15,7 +21,16 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/api/v1/stats", methods=['GET'], strict_slashes=False)
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
 def number_object():
     """retrieves the number of each objects by type"""
-    storage.count()
+    classes = {
+        "states": State,
+        "cities": City,
+        "amenities": Amenity,
+        "places": Place,
+        "reviews": Review,
+        "users": User
+    }
+    counts = {cls_key: storage.count(cls_val) for cls_key, cls_val in classes.items()}
+    return jsonify(counts)
