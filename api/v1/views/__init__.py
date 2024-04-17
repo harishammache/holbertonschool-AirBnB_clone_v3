31e1c7481c90ac90b38ec737e82cbd2eb039a121
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-04-17 14:48:55 +0000

    init with not circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
-from api.v1.views import states, cities, amenities, users
-from api.v1.views import places, places_reviews
-from api.v1.views.index import *
 from flask import Blueprint
-
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views import states, cities, amenities, users, places, places_reviews
+from api.v1.views.index import *
