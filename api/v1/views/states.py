7abfb530b9a48ab165536c5b265d903a35a943a4
harishammache <hammache_haris@hotmail.fr>
2024-04-17 12:39:31 +0000

    pycodestyle states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,12 +14,12 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
     """Retrieve a list of all State objects"""
-    states = storage.all(State) # recupère tous les objets de type State
-    state_list = [] # cree une liste vide
-    for state in states.values(): 
+    states = storage.all(State)  # recupère tous les objets de type State
+    state_list = []  # cree une liste vide
+    for state in states.values():
         # convertie en dictionnaire qui est ajouté a la liste
         state_list.append(state.to_dict())
-    return jsonify(state_list) # return la list convertie en dictionnaire json
+    return jsonify(state_list)  # return la list convertie en dictionnaire json
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -27,22 +27,23 @@ def get_states(state_id):
     """Retrieve a State object by ID"""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404) # Abandonne la requête avec une erreur 404
+        abort(404)  # Abandonne la requête avec une erreur 404
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """Delete a State object by ID"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    
+
     # Supprimer l'objet State de la base de données
     storage.delete(state)
     storage.save()
 
-    return {}, 200 # retourne un dictionnaire vide et un code 200
+    return {}, 200  # retourne un dictionnaire vide et un code 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
