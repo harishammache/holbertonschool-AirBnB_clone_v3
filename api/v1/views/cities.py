34e09c4f9a6986048069010641ea2bdc758abd92
harishammache <hammache_haris@hotmail.fr>
2024-04-17 13:49:05 +0000

    pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,7 +12,8 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<string:state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities_by_state(state_id):
     """Retrieve a list of all City objects for a specific State"""
     state = storage.get(State, state_id)
@@ -22,7 +23,7 @@ def get_cities_by_state(state_id):
     city_list = []
     for city in state.cities:
         city_list.append(city.to_dict())
-    
+
     return jsonify(city_list)
 
 
@@ -49,7 +50,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Create a new City in a specific State"""
     state = storage.get(State, state_id)
@@ -70,8 +72,8 @@ def create_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-
-@app_views.route('/cities/<string:city_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/cities/<string:city_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_city(city_id):
     """Update a City object"""
     city = storage.get(City, city_id)
